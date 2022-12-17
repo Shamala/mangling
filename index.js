2e0de12c5912423ae76d4ae57fb643f0d757f26e
Shamala Mallya <shamalamallya@Shamalas-MacBook-Air.local>
2022-12-16 16:45:56 -0800

    Updated error message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function mangling(string) {
-    if(typeof string !== 'string') throw new TypeError('Not s string');
+    if(typeof string !== 'string') throw new TypeError('Not a string');
     return string.replace(/\s/g,"");
 };
